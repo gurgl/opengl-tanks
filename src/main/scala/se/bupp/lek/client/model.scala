59eec7392f4685747985aedd2d5aa4bd4a1635c9
Kalle W <kalle.widell@gmail.com>
2012-10-20 06:20:49 +0200

    empy update quueu bug fix + shabby playerinfo relay to master server

---
+++ src/main/scala/se/bupp/lek/client/model.scala
@@ -0,0 +1,12 @@
+package se.bupp.lek.client
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: karlw
+ * Date: 2012-10-20
+ * Time: 00:11
+ * To change this template use File | Settings | File Templates.
+ */
+object Model {
+  type ClientPlayerInfo = String
+}
